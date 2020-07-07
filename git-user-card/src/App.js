5189d8acafc187033348d574731b4df19579894f
Joey <joey@xm8.ninja>
2020-07-07 17:30:05 -0400

    Finished MVP and Stretch by adding Github Contribution Graph

--- git-user-card/src/App.js
+++ git-user-card/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import GithubCard from "./Components/GithubCard";
+import Followers from "./Components/Followers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      githubData: {},
+      githubFollowers: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/Hoey1")
+      .then((res) => {
+        this.setState({
+          githubData: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get("https://api.github.com/users/Hoey1/followers")
+      .then((res) => {
+        this.setState({
+          githubFollowers: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div className="card">
+          <GithubCard githubData={this.state.githubData} />
+        </div>
+        <div className="header-followers">
+          <h2>Followers</h2>
+        </div>
+        <div className="follower">
+          {this.state.githubFollowers.map((follower) => {
+            return <Followers follower={follower} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
